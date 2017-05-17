7b9605b342d0ab1a42a91a69a081c394ab469031
John McIntosh II <jmci10@hotmail.com>
2017-05-17 02:08:51 +0000

    warn user of existing link

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -13,6 +13,9 @@ Template.postSubmit.events({
       if (error)
         return alert(error.reason);
         
+      if (result.postExists)
+        alert("This link has already been posted");
+        
       Router.go("postPage", {_id: result._id});
     });
   }
