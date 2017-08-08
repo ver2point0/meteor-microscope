49128c39a16bc3e75839cc4ff2d4983d2034c3bb
John McIntosh II <jmci10@hotmail.com>
2017-08-08 00:50:58 +0000

    create validatePost function

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,4 +1,17 @@
 /* global $ */
+Template.postSubmit.onCreated(function() {
+  Session.set("postSubmitErrors", {});
+});
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get("postSubmitErrors")[field];
+  },
+  errorClass: function(field) {
+    return !!Session.get("postSubmitErrors")[field] ? "has-error" : "";
+  }
+});
+
 Template.postSubmit.events({
   "submit form": function(e) {
     e.preventDefault();
