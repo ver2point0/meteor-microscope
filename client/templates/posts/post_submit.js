4ce4cd4d3c99814461c08c584f5e92fcf0dc7a60
John McIntosh II <jmci10@hotmail.com>
2017-08-08 00:54:47 +0000

    use validatePost function

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -21,13 +21,20 @@ Template.postSubmit.events({
       title: $(e.target).find("[name=title]").val()
     };
     
+    var errors = validatePost(post);
+    if (errors.title || errors.url) {
+      return Session.set("postSubmitErrors", errors);
+    }
+    
     Meteor.call("postInsert", post, function(error, result) {
       // display the error to the user and abort
-      if (error)
+      if (error) {
         return throwError(error.reason);
+      }
         
-      if (result.postExists)
+      if (result.postExists) {
         throwError("This link has already been posted");
+      }
         
       Router.go("postPage", {_id: result._id});
     });
